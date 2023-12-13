0bc0d4c27cfb606e30c191f8947916f3ea9b190c
soumaiaa <soumaiaalouii@gmail.com>
2023-12-13 19:15:21 +0100

    Exercice-PDO-1

--- index.php
+++ index.php
@@ -4,39 +4,46 @@ require_once('./database.php');
 
 $request = $database->query('SELECT * FROM clients');
 
-$clients = $request->fetchAll();
+$cliens = $request->fetchAll();
 
-var_dump($clients);
-// ex2
+// var_dump($clients);
+// // ex2
 $request = $database->query('SELECT * FROM showtypes');
 
 $spectacles = $request->fetchAll();
 
-var_dump($spectacles);
-// ex3
-$request = $database->query('SELECT * FROM clients  LIMIT 20');
+// var_dump($spectacles);
+// // ex3
+$request = $database->query('SELECT * FROM clients  LIMIT 0, 20');
 
-$clients = $request->fetchAll();
+$clientsVins = $request->fetchAll();
 
-var_dump($clients);
+// var_dump($clients);
 // ex4
-$request = $database->query('SELECT * FROM clients where card = TRUE');
+$request = $database->query('SELECT * FROM clients INNER JOIN cards ON  cards.cardNumber = clients.cardNumber WHERE cardTypesId=1');
 
-$clients = $request->fetchAll();
+$clints = $request->fetchAll();
 
-var_dump($clients);
-// ex5
-$request = $database->query('SELECT * FROM clients ORDER BY clients . lastName ASC');
+// var_dump($clients);
 
-$clients = $request->fetchAll();
+// // ex5
+$request = $database->query('SELECT * FROM clients  WHERE lastName LIKE "M%" ORDER BY clients.lastName');
 
-var_dump($clients);
+$clintes = $request->fetchAll();
+
+// var_dump($clients);
 // ex6
-$request = $database->query('SELECT * FROM clients');
+$request = $database->query('SELECT performer, title, date, startTime FROM shows ORDER BY title');
+
+$litles = $request->fetchAll();
+
+// var_dump($litles);
+// ex7
+$request = $database->query('SELECT * FROM clients LEFT JOIN cards ON  cards.cardNumber = clients.cardNumber');
 
 $clients = $request->fetchAll();
 
-var_dump($clients);
+// var_dump($clients);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -46,60 +53,78 @@ var_dump($clients);
     <title>Document</title>
 </head>
 <body>
-   <ul>
+  
        <?php
     //    ex1
-       foreach($clients as $client){
-        echo $client['lastName'] ." ". $client['firstName'];
+       echo "ex1 <br>";
+       foreach($cliens as $clien){
+        echo $clien['lastName'] ." ". $clien['firstName'] . "<br>";
        }
        echo '<hr>';
        echo '<hr>';
        //ex2
+       echo "ex2 <br>";
        foreach($spectacles as $spectacle){
-        echo $spectacle['type'] ;
+        echo $spectacle['type']."<br>" ;
        }
        echo '<hr>';
        echo '<hr>';
-      //ex3
-      foreach($clients as $client){
-        echo $clients;
-    }
+    //   ex3
+       echo "ex3 <br>";
+      foreach($clientsVins as $clientsVin){
+        echo $clientsVin['lastName'] . "<br>";
+       }
     echo '<hr>';
     echo '<hr>';
     //ex4
-    if($clients['card']===1){
-        echo $clients;
+    echo "ex4 <br>";
+    foreach($clints as $clint){
+        echo $clint['lastName'] ."<br>";
     }
+    echo '<hr>';
+    echo '<hr>';
     // ex5
-    foreach($clients as $client){
-    if($clients['lastName'[0]]=== "M"){
-       echo "Nom :" . $clients['lastName']."<br>"
-       . "Prénom :" . $clients['firstName']. "<br>";
- 
+    echo "ex5 <br>";
+    foreach($clintes as $clinte){
+       echo "Nom :" . $clinte['lastName']."<br>"
+       . "Prénom :" . $clinte['firstName']. "<br> <hr>";
+    }
+    echo '<hr>';
+    echo '<hr>';
+//    ex6
+    echo "ex6 <br>";
+    foreach ($litles as $litle){
+    echo $litle['title'] . " par " . $litle['performer'] . ", le ". $litle['date'] ." à " . $litle['startTime']."<br> <hr>";
+     }
+    echo '<hr>';
+    echo '<hr>';
+    // ex7
+    echo "ex7 <br>";
+    foreach ($clients as $client){
+    if($client['cardTypesId']===1){
+        echo "Nom :" . $client['lastName']."<br>"
+       . "Prénom :" . $client['firstName']. "<br>"
+       ."Date de naissance :" . $client['birthDate'] . "<br>"
+      . "Carte de fidélité : oui <br>" 
+      ."Numéro de carte :". $client['cardNumber'] ."<hr>";
+
+    }else if($client['cardTypesId']!=1 && $client['cardNumber']!=null){
+        echo "Nom :" . $client['lastName']."<br>"
+        . "Prénom :" . $client['firstName']. "<br>"
+        ."Date de naissance :" . $client['birthDate'] . "<br>"
+       . "Carte de fidélité : non <br>" 
+       ."Numéro de carte :". $client['cardNumber']. "<hr>";
+       } else if( $client['cardNumber']===null){
+        echo "Nom :" . $client['lastName']."<br>"
+        . "Prénom :" . $client['firstName']. "<br>"
+        ."Date de naissance :" . $client['birthDate'] . "<br>"
+       . "Carte de fidélité : non <br>" 
+       ."Numéro de carte :null <hr>";  
     }
 }
-//    ex6
-foreach($clients as $client){
-    if($clients['card']=== 1){
-       echo "Nom :" . $clients['lastName']."<br>"
-       . "Prénom :" . $clients['firstName']. "<br>"
-       ."Date de naissance :" . $clients['birthDate'] . "<br>"
-      . "Carte de fidélité : oui" 
-      ."Numéro de carte :". $clients['cardNumber'];
-
-    }elseif ($clients['card']=== 0){
-        echo "Nom :" . $clients['lastName']."<br>"
-        . "Prénom :" . $clients['firstName']. "<br>"
-        ."Date de naissance :" . $clients['birthDate'] . "<br>"
-       . "Carte de fidélité : non" 
-       ."Numéro de carte :". $clients['cardNumber'];
-       }
-       
-    }
-    
 
 
        ?>
-   </ul> 
+   
 </body>
 </html>
